09e9e8e33e4dd9a1b6bda431404c73856a8f75fe
rmanzi13 <reto2013@gmail.com>
2024-08-23 11:52:50 +0200

    Configuración inicial del proyecto React

---
+++ web-cantautora/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Website Cantautora</h1>
+      <p>Bienvenido al sitio web de la cantautora.</p>
+    </div>
+  );
+}
+
+export default App;
